f647874d190ee1897fa99aa1723f363b3111ebdd
Max Edmonds <maxedmonds05@gmail.com>
2020-08-23 16:25:06 -0700

    Implement multiple guest entry and guestlist/event_id association

--- assets/js/guest-create.js
+++ assets/js/guest-create.js
@@ -1,19 +1,24 @@
 const createYourGuestList = $("#createYourGuestList");
 
-let i = 0;
+let dataFetchers = [];
 
-function gatherFormData() {
-  const guestName = $("#guestName").val();
-  const guestEmail = $("#guestEmail").val();
-  const guestPhoneNumber = $("#guestPhoneNumber").val();
+var search = location.search.substring(1);
+// eslint-disable-next-line
+search = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
 
-  const guestObject = {
-    guestName: guestName,
-    guestEmail: guestEmail,
-    guestPhoneNumber: guestPhoneNumber
-  };
-  return guestObject;
-}
+// function gatherFormData() {
+//   const guestName = $("#guestName").val();
+//   const guestEmail = $("#guestEmail").val();
+//   const guestPhoneNumber = $("#guestPhoneNumber").val();
+//   //need to grab Event // where is the event id at the time of submitting form?
+//   const guestObject = {
+//     guestName: guestName,
+//     guestEmail: guestEmail,
+//     guestPhoneNumber: guestPhoneNumber,
+//     EventId: search.event_id
+//   };
+//   return guestObject;
+// }
 
 // function gatherFormData() {
 //   let formDataObject = {};
@@ -34,34 +39,49 @@ function gatherFormData() {
 // }
 
 function guestCreate() {
-  const newGuest = gatherFormData();
-  $.ajax({
-    method: "POST",
-    url: "/api/guest-create",
-    data: newGuest
+  dataFetchers.forEach(function (dataFetcher) {
+    const newGuest = dataFetcher();
+    $.ajax({
+      method: "POST",
+      url: "/api/guest-create",
+      data: newGuest
+    });
   });
   window.location.pathname = "/viewEvent";
 }
 
+function addGuestInput(event) {
+  if (event) {
+    event.preventDefault();
+  }
+  let formRow = $(`<div class="form-row">`);
+  let nameFormGroup = $(`<div class="form-group col-4">`);
+  let nameLabel = $(`<label for="inputGuestName${dataFetchers.length}">`).text("Name");
+  let nameInput = $(`<input type="text" class="form-control" id="inputGuestName${dataFetchers.length}">`);
+  nameFormGroup.append(nameLabel, nameInput);
+  let emailFormGroup = $(`<div class="form-group col-4">`);
+  let emailLabel = $(`<label for="inputGuestEmail${dataFetchers.length}">`).text("Email");
+  let emailInput = $(`<input type="text" class="form-control" id="inputGuestEmail${dataFetchers.length}">`);
+  emailFormGroup.append(emailLabel, emailInput);
+  let phoneFormGroup = $(`<div class="form-group col-4">`);
+  let phoneLabel = $(`<label for="inputGuestPhone${dataFetchers.length}">`).text("Phone Number");
+  let phoneInput = $(`<input type="text" class="form-control" id="inputGuestPhone${dataFetchers.length}">`);
+  phoneFormGroup.append(phoneLabel, phoneInput);
+  formRow.append(nameFormGroup, emailFormGroup, phoneFormGroup);
+  $("#guests").append(formRow);
+  dataFetchers.push(function () {
+    return {
+      guestName: nameInput.val(),
+      guestEmail: emailInput.val(),
+      guestPhoneNumber: phoneInput.val(),
+      EventId: search.event_id
+    };
+  });
+}
+addGuestInput();
+
 // Add another guest input form
-$(addGuest).click(function () {
-  event.preventDefault();
-  i++;
-  $(createGuestList).append(
-    `<div class="form-group col-4">
-      <label for="inputGuestName">Name</label>
-      <input type="text" class="form-control" id="guestName${i}">
-    </div>
-    <div class="form-group col-4">
-      <label for="inputGuestEmail">Email</label>
-      <input type="text" class="form-control" id="guestEmail${i}">
-    </div>
-    <div class="form-group col-4">
-      <label for="inputGuestPhoneNumber">Phone Number</label>
-      <input type="text" class="form-control" id="guestPhoneNumber${i}">
-    </div>`
-  );
-});
+$(addGuest).click(addGuestInput);
 
 // Create Event
 $(createYourGuestList).click(function () {
